036dccef895daf0426bc60d1e401c65373cbe46e
dineshkumar-mca-23 <139850007+dineshkumar-mca-23@users.noreply.github.com>
2024-03-21 11:49:02 +0530

    Add files via upload
    client bank

---
+++ src/home.js
@@ -0,0 +1,15 @@
+export default function Home(){
+    return(
+        <>
+         <div id="home" className="transparent-box">
+      <h3>Welcome to MCA - BANK</h3>
+      <h4>"Online banking"</h4>
+      <p id="span-1" > Available 24/7 from almost any device with an internet connection</p>
+      <p id="span-2">Create new Account ,balance checks and deposit your money.</p>
+         </div>
+
+  
+       
+        </>
+    )
+}
